7160b132f22a899afb28c6e41bee2873241f9e09
pan <panjunzhong@outlook.com>
2021-12-14 21:10:10 +0800

    ui: complete page Now

--- miniprogram/pages/now/now.js
+++ miniprogram/pages/now/now.js
@@ -5,11 +5,22 @@ Page({
    */
   data: {
     lists: [
-      { description: "位置", list: [] },
-      { description: "时间", list: [] },
-      { description: "超限", list: [] },
-      { description: "最近添加", list: [] },
+      { description: "超限", fold: false, list: [] },
+      { description: "位置", fold: false, list: [] },
+      { description: "时间", fold: false, list: [] },
+      { description: "最近添加", fold: false, list: [] },
     ]
+  },
+  foldExpand: function(e) {
+    // console.log(e);
+    let i = e.currentTarget.dataset.index
+    // console.log(i);
+    let b = !this.data.lists[i].fold;
+    let temp = 'lists[' + i + '].fold';
+    this.setData({
+      [temp]: b,
+    })
+    // console.log(i + ', ' + this.data.lists[i].fold);
   },
   /**
    * @param {string} title 
@@ -20,23 +31,24 @@ Page({
    * @param {bool} outofdate 
    * @param {int} which 
    */
-  additem: function(title, project, tag, date, outofdate, which, iid = 0) {
+  additem: function(title, project, tag, date, outofdate, waiting, which, iid = 0) {
     this.data.lists[which].list.push({
       title: title,
       project: project,
       tag: tag,
       date: date,
       outofdate: outofdate,
+      waiting: waiting,
       iid: iid,
     })
   },
-  locin: 0,
-  timein: 1,
-  expirein: 2,
+  expirein: 0,
+  locin: 1,
+  timein: 2,
   recentin: 3,
   sortlilsts: function() {
     this.data.lists.forEach(e => {
-      e.list.sort((a, b) => { return a.date - b.date })
+      e.list.sort((a, b) => a.date.localeCompare(b.date))
     });
     this.data.lists[this.recentin].list.reverse();
   },
@@ -44,19 +56,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    this.additem("学nosql存储模式", "微信小程序", '工作', '2021-12-14', false, this.locin);
-    this.additem("借数据库教材", "数据库期末", '出行', '2021-12-14', false, this.timein);
-    this.additem("打电话给非洲", "非洲水电", '工作', '2021-12-12', true, this.expirein);
-    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-14', false, this.recentin);
-    this.additem("学nosql存储模式", "微信小程序", '工作', '2021-12-14', false, this.locin);
-    this.additem("借数据库教材", "数据库期末", '出行', '2021-12-14', false, this.timein);
-    this.additem("打电话给非洲", "非洲水电", '工作', '2021-12-12', true, this.expirein);
-    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-14', false, this.recentin);
-    this.additem("学nosql存储模式", "微信小程序", '工作', '2021-12-14', false, this.locin);
-    
-    this.additem("借数据库教材", "数据库期末", '出行', '2021-12-14', false, this.timein);
-    this.additem("打电话给非洲", "非洲水电", '工作', '2021-12-12', true, this.expirein);
-    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-14', false, this.recentin);
+    this.additem("学nosql存储模式", "微信小程序", '工作', '2022-12-14', false, false, this.locin);
+    this.additem("借数据库教材", "数据库期末", '出行', '2021-12-14', false, false, this.timein);
+    this.additem("打电话给非洲", "非洲水电", '工作', '2020-12-12', true, false, this.expirein);
+    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-14', false, false, this.recentin);
+    this.additem("学nosql存储模式", "微信小程序", '工作', '2018-12-14', false, true, this.locin);
+    this.additem("借数据库教材", "数据库期末", '出行', '2017-12-14', false, false, this.timein);
+    this.additem("打电话给非洲", "非洲水电", '工作', '2016-12-12', true, false, this.expirein);
+    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-11', false, false, this.recentin);
+    this.additem("学nosql存储模式", "微信小程序", '工作', '2015-12-14', false, false, this.locin);
+    this.additem("借数据库教材", "数据库期末", '出行', '2021-12-12', false, false, this.timein);
+    this.additem("打电话给非洲", "非洲水电", '工作', '2021-12-14', true, true, this.expirein);
+    this.additem("打灰", "洲际导弹订单", '军工', '2021-12-14', false, false, this.recentin);
     this.sortlilsts();
     this.setData({
       lists: this.data.lists,
