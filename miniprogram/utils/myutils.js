1c1a2f452874f3834ade64e692d0bb9add6db9a5
pan <panjunzhong@outlook.com>
2021-12-19 10:58:24 +0800

    refactor(data): rewrite data logic, remove all oo design

--- miniprogram/utils/myutils.js
+++ miniprogram/utils/myutils.js
@@ -85,121 +85,12 @@ exports.DateUtil = {
 }
 
 exports.TodoData = {
-  Sub: function() {
-    this.iid = ''
-    this.pid = ''
-    this.title = ''
-    this.duedate = ''
-    this.triggerdate = ''
-    this.prereq = {}
-    this.notify = []
-    this.tag = ''
-    this.scene = ''
-    this.done = false
-    this.viewdata = {
-      /**
-       * datestr
-       * outofdate
-       * waiting
-       * projectname
-       */
-    }
-  },
-  createSub: function(pid, title, duedate, triggerdate, tag, prereq = [], scene = "") {
-    var r = new this.Sub();
-    r.pid = pid;
-    r.title = title;
-    r.duedate = duedate;
-    r.triggerdate = triggerdate;
-    r.tag = tag;
-    r.prereq = prereq;
-    r.scene = scene;
-    r.iid = exports.DateUtil.genUID('sub');
-    return r;
-  },
-
-  Project: function() {
-    this.pid = '';
-    this.title = '';
-    this.duedate = '';
-    this.subs = [];
-    this.progress = 0;
-    this.cando = 0;
-    this.waiting = 0;
-    this.done = 0;
-    this.outofdate = 0;
-    this.total = 0;
-    this.viewdata = {
-      /**
-       * datestr
-       * countdown
-       */
-      datestr: 'XXXXXX',
-      countdown: 0,
-      score: 'S+',
-    };
-  },
-  // TODO: use better reviewing method
-  // an alternative is to use quantitative scoring,
-  // then convert to grade score in the view.
-  _score_algorithm = function(done, ood, total, expired) {
-    if (expired) return 'F-';
-    if (ood > total / 2) return 'F';
-    if (done > total / 2) {
-      if (ood > total / 3) {
-        return 'A+';
-      } else if (ood > 0) {
-        return 'S';
-      } else {
-        return 'S+';
-      }
-    } else {
-      if (ood > 0) {
-        return 'A-';
-      } else {
-        return 'A'
-      }
-    }
-  }
-  /**
-   * 4 args
-   * @param {String} title 
-   * @param  duedate timestamp(Date().getTime)
-   * @param {Array} subs 
-   * 
-   * @param {Number} cando 
-   * @param {Number} ood 
-   * @param {Number} wait 
-   * functions: <br/>
-   * setPid()<br/>
-   * updateView(mapSub)<br/>
-   * removeSub(iid)<br/>
-   */
-  createProject: function(title, duedate, subs = []) {
-    console.log(subs);
-    var p = new this.Project();
-    p.pid = exports.DateUtil.genUID('pro');
-    p.title = title;
-    p.duedate = duedate;
-    p.subs = subs;
-    return p;
-  },
-  Location: function(lat = 0, lon = 0, name = '') {
+  Location: function(lat, lon, name) {
     this.lat = lat;
     this.lon = lon;
     this.name = name;
-    this.distanceTo = function(rhs) {
-      return this._distance(lat, rhs.lat, lon, rhs.lon);
-    }
-    this.inRangeOf = function(start_loc, radius = 0.1) {
-      var km = this.distanceTo(start_loc);
-      if (km <= radius) {
-        return true;
-      }
-      return false;
-    }
   },
-  distance = function(loc1, loc2) {
+  distance: function(loc1, loc2) {
     var lon1 = loc1.lon;
     var lon2 = loc2.lon;
     var lat1 = loc1.lat;
@@ -211,42 +102,193 @@ exports.TodoData = {
     lon2 = lon2 * Math.PI / 180;
     lat1 = lat1 * Math.PI / 180;
     lat2 = lat2 * Math.PI / 180;
-
     // Haversine formula
     let dlon = lon2 - lon1;
     let dlat = lat2 - lat1;
     let a = Math.pow(Math.sin(dlat / 2), 2) +
       Math.cos(lat1) * Math.cos(lat2) *
       Math.pow(Math.sin(dlon / 2), 2);
-
     let c = 2 * Math.asin(Math.sqrt(a));
-
     // Radius of earth in kilometers. Use 3956
     // for miles
     let r = 6371;
-
     // calculate the result
-    return (c * r);
+    return (c * r) * 1000;
   },
-  Tag: function() {
+  /**
+   * 
+   * @param {String} name 
+   * @param {Array<Number>} time arr of int 
+   * @param {Array<Location>} location arr of Location
+   */
+  Tag: function(name = '', time = [], location = []) {
     // tag won't have id
     // we need name check
     // in the view model.
     // f**c javascript
-    if (arguments.length == 1) {
-      this.name = arguments[0];
-    } else {
-      this.name = '';
-    }
-    if (arguments.length == 2) {
-      this.time = arguments[1];
-    } else { this.time = []; }
-    if (arguments.length == 3) {
-      this.location = arguments[2];
-    } else {
-      this.location = [];
+    this.name = name;
+    this.time = time;
+    this.location = location;
+  },
+
+  /**
+   * 
+   * @param {String} pid 
+   * @param {String} title 
+   * @param {Date} duedate 
+   * @param {Date} triggerdate 
+   * @param {String} tag tagname!
+   * @param {Map<String, bool>} prereq 
+   * @param {*} scene 
+   */
+  Sub: function(pid, title, duedate, triggerdate = exports.getToday(), tag = null, prereq = {}, scene = null) {
+    this.iid = exports.DateUtil.genUID('sub');
+    this.pid = pid
+    this.title = title
+    this.duedate = duedate
+    this.triggerdate = triggerdate
+    this.prereq = prereq
+    this.notify = []
+    this.tag = tag
+    this.scene = ''
+    this.done = false
+    this.viewdata = {
+      datestr: '1999-05-04',
+      triggerstr: '1999-05-04',
+      outofdate: false,
+      waiting: true,
+      projectname: pid,
     }
   },
+  _get_pre_state: function(sMap, sub) {
+    var alldone = true;
+    for (var piid in sub.prereq) {
+      sub.prereq[piid] = sMap(piid).done;
+      alldone |= sub.prereq[piid];
+    }
+    return alldone;
+  },
+  initSubView: function(pMap, sMap, sub) {
+    var today = exports.DateUtil.getToday();
+    sub.duedate = new Date(sub.duedate);
+    sub.viewdata.datestr = exports.DateUtil.dateToViewString(today);
+    sub.viewdata.outofdate = exports.DateUtil.isNowAfter(today);
+    sub.viewdata.projectname = pMap(sub.pid).title;
+    var td = new Date(sub.triggerdate);
+    sub.triggerdate = td;
+    this.updateSubWaiting(sMap, sub);
+  },
+  updateSubWaiting: function(sMap, sub) {
+    sub.viewdata.waiting = !this._get_pre_state(sMap, sub) || exports.DateUtil.isNowAfter(sub.triggerdate);
+    return sub.viewdata.waiting;
+  },
+  addPrereqState: function(sub, pre, done = false) {
+    var preiid = pre.iid;
+    // add a prereq in my req list
+    sub.prereq[preiid] = done;
+    // add me to prereq's notify list
+    pre.notify.push(sub.iid);
+  },
+  removePrereq: function(sub, pre) {
+    // remove piid from my pre list
+    var preiid = pre.iid;
+    delete sub.prereq[preiid];
+    // remove me from pre's notify list
+    var s = new Set(pre.notify);
+    s.delete(sub.iid);
+    pre.notify = Array.from(s);
+  },
+  doneEntry: function(SubMap, sub) {
+    // notify who depend on me
+    sub.notify.forEach(n => {
+      SubMap(n).prereq[sub.iid] = true;
+    })
+  },
+  undoneEntry: function(SubMap, sub) {
+    // notify who depend on me
+    sub.notify.forEach(n => {
+      SubMap(n).prereq[sub.iid] = false;
+    })
+  },
+  Project: function(title = '', duedate = '', subs = []) {
+    this.pid = exports.DateUtil.genUID('pro');
+    this.title = title;
+    this.duedate = duedate;
+    this.subs = subs;
+    this.progress = 0;
+    this.cando = 0;
+    this.waiting = 0;
+    this.done = 0;
+    this.outofdate = 0;
+    this.total = 0;
+    this.viewdata = {
+      datestr: 'XXXXXX',
+      countdown: 0,
+      score: 'S+',
+    };
+  },
+  addSub: function(p, iid) {
+    p.subs.push(iid);
+  },
+  // TODO: use better reviewing method
+  // an alternative is to use quantitative scoring,
+  // then convert to grade score in the view.
+  score: function(p) {
+    var done = p.done,
+      ood = p.outofdate,
+      total = p.total,
+      expired = p.viewdata.countdown < 0;
+    var sc;
+    if (expired) sc = 'F-';
+    if (ood > total / 2) sc = 'F';
+    if (done > total / 2) {
+      if (ood > total / 3) {
+        sc = 'A+';
+      } else if (ood > 0) {
+        sc = 'S';
+      } else {
+        sc = 'S+';
+      }
+    } else {
+      if (ood > 0) {
+        sc = 'A-';
+      } else {
+        sc = 'A'
+      }
+    }
+    p.viewdata.score = sc;
+    return sc;
+  },
+  initProjectView: function(subMap, p) {
+    this.updateProjectView(subMap, p);
+    var today = exports.DateUtil.getToday();
+    p.duedate = new Date(p.duedate);
+    p.viewdata.datestr = exports.DateUtil.dateToViewString(p.duedate);
+    p.viewdata.countdown = exports.DateUtil.expireDays(p.duedate);
+    p.viewdata.score = this.score(p);
+  },
+  /**
+   * you should only update p.waiting, p.done, p.cando in setData list
+   */
+  updateProjectView: function(subMap, p) {
+    var waiting = 0,
+      done = 0,
+      total = p.subs.length;
+    var _this = this;
+
+    p.subs.forEach(s => {
+      if (_this.updateSubWaiting(subMap, subMap(s))) {
+        waiting += 1;
+      }
+      if (s.done) {
+        done += 1;
+      }
+    })
+    p.waiting = waiting;
+    p.done = done;
+    p.cando = total - done - waiting;
+    p.total = total;
+  }
 }
 
 exports.UserData = {
